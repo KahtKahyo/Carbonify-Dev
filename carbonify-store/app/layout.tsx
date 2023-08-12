45689cbf31e419a2b07ed56d68be5ed6c655a865
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-08-13 00:43:55 +0700

    minor changes

--- carbonify-store/app/layout.tsx
+++ carbonify-store/app/layout.tsx
@@ -10,8 +10,8 @@ import './globals.css'
 const font = Urbanist({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Store',
-  description: 'Store - The place for all your purchases.',
+  title: 'Carbonify Store',
+  description: 'Carbonify Store',
 }
 
 export default function RootLayout({
